c1e052ad4d1ab27e97a8667964ba482c4bf9ad41
YourDad07 <salmanadhikari415@gmail.com>
2022-07-17 11:28:56 +0530

    Added The Route For Testing

--- main.go
+++ main.go
@@ -19,6 +19,9 @@ func main() {
 	fmt.Println("Server Starting")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", controllers.Home).Methods("GET")
+	router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	}).Methods("GET")
 	router.HandleFunc("/track", controllers.Track).Methods("POST")
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
